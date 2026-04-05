81f5afa584fd15a9ea225f7e59a8c7f87e9fd130
Tanner Allen <tlallen1419@gmail.com>
2026-04-05 22:30:50 +0000

    add CORS middleware to server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { db, User, Task } = require('./database/setup');
@@ -8,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // JWT Authentication Middleware
